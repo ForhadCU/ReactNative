4c7d25e146bd376145fc92162c323bfaee001812
Forhad Kashem <kashem.tipo@gmail.com>
2020-11-25 22:31:44 +0600

    Updated

--- ProWithMosh_RN/App.js
+++ ProWithMosh_RN/App.js
@@ -1,23 +1,41 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import {
   SafeAreaView,
-  StyleSheet,
-  View,
-  TextInput,
-  Button,
+  ScrollView,
   Text,
+  TouchableOpacity,
+  View,
 } from 'react-native';
-import BasicTest from './myCodes/components/BasicTest';
-import BasicTest2 from './myCodes/components/BasicTest2'
-
 import styles from '/home/forhad/ReactNative/ProWithMosh_RN/myCodes/components/configs/StyleSheet.js';
 
-
 const App = () => {
-    return (
+  function onclick() {
+    try {
+      fetch('https://webhook.site/80fe2e69-2aa0-4051-a61b-62228b8c5ecc', {
+        method: 'post',
+        mode: 'no-cors',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          firstName: 'Forhad',
+          lastName: 'Kashem',
+        }),
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
     <SafeAreaView style={[styles.container]}>
-      <BasicTest />
-      <BasicTest2 />
+      <TouchableOpacity
+        onPress={() => {
+          onclick();
+        }}>
+        <Text>This is testing</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 };
